d19bb7da16aecda47dbdee747f3d8eb826d27e8e
root <root@netboot-cluster-test>
2019-08-20 19:39:50 -0700

    Live comment stream tool.

---
+++ tools/commentstream.py
@@ -0,0 +1,25 @@
+import redis
+import json
+
+r = redis.StrictRedis()
+p = r.pubsub(ignore_subscribe_messages=True)
+
+def handle_rpan_event(event):
+    data = json.loads(event["data"])
+    try:
+        payload = data['payload']
+    except KeyError:
+        return
+    # print(data)
+    try:
+        print(f"[{payload['link_id']}] {payload['author']}: {payload['rtjson']['document'][0]['c'][0]['t']}")
+    except KeyError:
+        print(payload)
+
+p.psubscribe(**{"rpan:events:*": handle_rpan_event})
+thread = p.run_in_thread(sleep_time=0.001)
+
+try:
+    thread.join()
+except KeyboardInterrupt:
+    thread.stop()
